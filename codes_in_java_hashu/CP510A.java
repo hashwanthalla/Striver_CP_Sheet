6c495045785899c916ff5501ca8573f921cd9232
halla <halla@castlighthealth.com>
2022-11-07 10:19:34 +0530

    510A

---
+++ codes_in_java_hashu/CP510A.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class CP510A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(i%2!=0)
+                {
+                    System.out.print('#');
+                }
+                else
+                {
+                    if(i%2==0&&i%4!=0)
+                    {
+                        if(j!=m)
+                        {
+                            System.out.print('.');
+                        }
+                        else
+                        {
+                            System.out.print('#');
+                        }
+                    }
+                    else
+                    {
+                        if(j!=1)
+                        {
+                            System.out.print('.');
+                        }
+                        else
+                        {
+                            System.out.print('#');
+                        }
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
